9786ab45e60cc4f221d8f0f730a4489a6a3da944
Iyan Sanchez <iyannsanchezzdc4@gmail.com>
2024-10-07 21:45:32 +0200

    Row and columns on js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Square from './Square';
+
 
 function App() {
+  const [currentAtempt, setCurrentAtempt] = useState(1)
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p id='headerName'>
+          WORDLE
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div className='board'>
+        <div className='row'>
+          <Square row="1" column="1" attempt={currentAtempt}/>
+          <Square row="1" column="2" attempt={currentAtempt}/>
+          <Square row="1" column="3" attempt={currentAtempt}/>
+          <Square row="1" column="4" attempt={currentAtempt}/>
+          <Square row="1" column="5" attempt={currentAtempt}/>
+        </div>
+        <div className='row'>
+          <Square row="2" column="1" attempt={currentAtempt}/>
+          <Square row="2" column="2" attempt={currentAtempt}/>
+          <Square row="2" column="3" attempt={currentAtempt}/>
+          <Square row="2" column="4" attempt={currentAtempt}/>
+          <Square row="2" column="5" attempt={currentAtempt}/>
+        </div>
+        <div className='row'>
+          <Square row="3" column="1" attempt={currentAtempt}/>
+          <Square row="3" column="2" attempt={currentAtempt}/>
+          <Square row="3" column="3" attempt={currentAtempt}/>
+          <Square row="3" column="4" attempt={currentAtempt}/>
+          <Square row="3" column="5" attempt={currentAtempt}/>
+        </div>
+        <div className='row'>
+          <Square row="4" column="1" attempt={currentAtempt}/>
+          <Square row="4" column="2" attempt={currentAtempt}/>
+          <Square row="4" column="3" attempt={currentAtempt}/>
+          <Square row="4" column="4" attempt={currentAtempt}/>
+          <Square row="4" column="5" attempt={currentAtempt}/>
+        </div>
+        <div className='row'>
+          <Square row="5" column="1" attempt={currentAtempt}/>
+          <Square row="5" column="2" attempt={currentAtempt}/>
+          <Square row="5" column="3" attempt={currentAtempt}/>
+          <Square row="5" column="4" attempt={currentAtempt}/>
+          <Square row="5" column="5" attempt={currentAtempt}/>
+        </div>
+      </div>
     </div>
   );
 }
